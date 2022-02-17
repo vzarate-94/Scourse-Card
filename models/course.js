2a3dffb277535fd6dbe39310c4525fc539905e4d
vzarate26 <vzarate26@gmail.com>
2022-02-17 17:53:04 -0500

    add ternary function to add default img src if course is added without one

--- models/course.js
+++ models/course.js
@@ -6,6 +6,8 @@ export {
 
 const Schema = mongoose.Schema
 
+let url = "https://www.hotelroanoke.com/i/SITE_160212_10485664_PSYN1/content/CMS_02122016_105756563_RS0G0/78380B0E-983D-0F11-C5E92761DED6C56D.JPG"
+
 const reviewSchema = new mongoose.Schema({
   reviewer: String,
   content: String,
@@ -20,7 +22,7 @@ const courseSchema = new mongoose.Schema({
   },
   city: String,
   state: String,
-  img: String,
+  img: {type: String, default: url},
   reviews: [reviewSchema],
   owner: {type: mongoose.Schema.Types.ObjectId, ref: "Profile"}
 }, {
